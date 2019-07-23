ffb10779fc05cfa8ab244141fca7ad6167b6e462
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-07-23 17:00:16 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES
+    ("Jordan", 3, "male", "brown", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("Mary", 4, "female", "yellow", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("Jord", 3, "male", "black", "nice", "yes");
+
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("Jeremy", 5, "male", "green", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("val", 7, "male", "brown", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("Jordan", 3, "male", "brown", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    ("Greg", 8, "male", "brown", "nice", "yes");
+
+INSERT INTO 
+    bears(name, age, gender, color, temperament, alive)
+VALUES  
+    (null, 3, "male", "brown", "nice", "yes");
+
+
+
